eb6a584ba6b38a1a6147ed58574927f6d89d01a0
DeanWexler <xocochayden4@gmail.com>
2025-05-28 22:52:14 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { cartographer } from "@replit/vite-plugin-cartographer";
+
+const rootDir = decodeURIComponent(new URL(".", import.meta.url).pathname);
+
+export default defineConfig({
+  plugins: [
+    react(),
+    runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
+      ? [cartographer()]
+      : []),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(rootDir, "client", "src"),
+      "@shared": path.resolve(rootDir, "shared"),
+      "@assets": path.resolve(rootDir, "attached_assets"),
+    },
+  },
+  root: path.resolve(rootDir, "client"),
+  build: {
+    outDir: path.resolve(rootDir, "dist/public"),
+    emptyOutDir: true,
+  },
+});
